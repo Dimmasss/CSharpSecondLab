30f715b88887b0c9010906fa1e341d7bb05b2d15
Dimmasss <bogdansukailo00kx@gmail.com>
2024-03-16 16:04:36 +0200

    second lab

---
+++ SukailoCSharpLab2/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using SukailoCSharpLab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SukailoCSharpLab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private async void ProceedButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CalculateAge())
+            {
+                FirsNameAnswer.Content = "Name : ";
+                LastNameAnswer.Content = "Surname : " ;
+                EmailAddressAnswer.Content = "E-mail address: " ;
+                DateOfBirthAnswer.Content = "Date of birth: " ;
+                WhetherIsAdult.Text = "Is adult?: " ;
+                SunSign.Text = "Sun sign: " ;
+                ChineeseSign.Text = "Chinese sign: ";
+                WhetherIsBirthday.Text = "Is birthday: " ;
+                return;
+            }
+            Person persn = new Person(FirstName.Text, LastName.Text, Email.Text, DatePicker.SelectedDate.Value);
+            await persn.AsynchronInit();
+            FirsNameAnswer.Content = "Name : " + persn.FirstName;
+            LastNameAnswer.Content = "Surname : " + persn.LastName;
+            EmailAddressAnswer.Content = "E-mail address: " + persn.EmailAddress;
+            DateOfBirthAnswer.Content = "Date of birth: " + persn.DateOfBirth.ToShortDateString();
+            WhetherIsAdult.Text = "Is adult?: " + persn.IsAdult;
+            SunSign.Text = "Sun sign: " + persn.SunSign;
+            ChineeseSign.Text = "Chinese sign: " + persn.ChineseSign;
+            WhetherIsBirthday.Text = "Is birthday: " + persn.IsBirthday;
+
+
+        }
+        private bool CalculateAge()
+        {
+            DateTime? dateOfBirth = DatePicker.SelectedDate;
+            if (dateOfBirth != null)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - dateOfBirth.Value.Year;
+                if (dateOfBirth > today.AddYears(-age)) age--;
+                if (age < 0)
+                {
+                    MessageBox.Show("User did not born!");
+                }
+                else if(age > 135)
+                {
+                    MessageBox.Show("User cannot be over 135!");
+                }
+                else
+                {
+                    if (dateOfBirth.Value.Month == today.Month && dateOfBirth.Value.Day == today.Day)
+                        MessageBox.Show("Happy Birthday!");
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void LastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+    }
+
+    
+}
---
+++ SukailoCSharpLab2/Models/Person.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SukailoCSharpLab2.Models
+{
+    internal class Person
+    {
+        #region Fields
+        private string _firstName;
+        private string _lastNname; 
+        private DateTime _dateOfBirth;
+        private string _emailAddress;
+        private  bool _isAdult;
+        private  string _sunSign;
+        private  string _chineseSign;
+        private  bool _isBirthday;
+        #endregion
+
+        #region Properties
+        // Властивості
+        public string FirstName { get; private set; } 
+        public string LastName { get; private set; } 
+        public string EmailAddress { get; private set; } 
+        public DateTime DateOfBirth { get; private set; }
+        public bool IsAdult { get; private set; }
+        public string SunSign { get; private set; }
+        public string ChineseSign { get; private set; }
+        public bool IsBirthday { get; private set; }
+        #endregion
+
+        #region Constructors
+        // Конструктор, що приймає всі чотири параметри
+        public Person(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = email;
+            DateOfBirth = dateOfBirth;
+        }
+
+        // Конструктор, що приймає ім'я, прізвище та адресу електронної пошти
+        public Person(string firstName, string lastName, string emailAddress) : this(firstName, lastName, emailAddress, DateTime.MinValue)
+        { }
+
+        // Конструктор, що приймає ім'я, прізвище та дату народження
+        public Person(string firstName, string lastName, DateTime dateOfBirth) : this(firstName, lastName, "", dateOfBirth)
+        { }
+        #endregion
+
+        #region Calculations
+        public async Task AsynchronInit()
+        {
+            await Task.Run(() =>
+            {
+                IsAdult = CalcIsAdult();
+                SunSign = CalcSunSign();
+                ChineseSign = CalcChineseSign();
+                IsBirthday = CalcIsBirthday();
+            });
+        }
+
+        private bool CalcIsAdult() {
+            DateTime now = DateTime.Now;
+            int userAge = now.Year - DateOfBirth.Year;
+
+            if (now.DayOfYear < DateOfBirth.DayOfYear)
+            {
+                userAge -= 1;
+            }
+
+            return userAge >= 18;
+        }
+
+        private string CalcSunSign()
+        {
+            int month = DateOfBirth.Month;
+            int day = DateOfBirth.Day;
+
+            switch (month)
+            {
+                case 1:
+                    return (day <= 19) ? "Capricorn" : "Aquarius";
+                case 2:
+                    return (day <= 18) ? "Aquarius" : "Pisces";
+                case 3:
+                    return (day <= 20) ? "Pisces" : "Aries";
+                case 4:
+                    return (day <= 20) ? "Aries" : "Taurus";
+                case 5:
+                    return (day <= 20) ? "Taurus" : "Gemini";
+                case 6:
+                    return (day <= 20) ? "Gemini" : "Cancer";
+                case 7:
+                    return (day <= 22) ? "Cancer" : "Leo";
+                case 8:
+                    return (day <= 22) ? "Leo" : "Virgo";
+                case 9:
+                    return (day <= 22) ? "Virgo" : "Libra";
+                case 10:
+                    return (day <= 22) ? "Libra" : "Scorpio";
+                case 11:
+                    return (day <= 21) ? "Scorpio" : "Sagittarius";
+                default:
+                    return (day <= 21) ? "Sagittarius" : "Capricorn";
+            }
+        }
+
+        private string CalcChineseSign()
+        {
+            int year = DateOfBirth.Year;
+            string[] chineseHoroscop = { "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox","Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat" };
+
+            return chineseHoroscop[year % 12];
+        }
+
+        private bool CalcIsBirthday() { return DateTime.Now.Month == DateOfBirth.Month && DateTime.Now.Day == DateOfBirth.Day; }
+
+
+        #endregion
+    }
+}
